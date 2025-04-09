8ea0f9259f23b2a2cc381492f3269b79d001c0f7
Pranay-Erra <pranayerra2003@gmail.com>
2025-04-09 07:40:15 +0530

    deployed  POC

--- ui/src/Login/LoginPage.js
+++ ui/src/Login/LoginPage.js
@@ -21,7 +21,7 @@ const LoginPage = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await fetch('http://localhost:5000/api/login', {
+      const response = await fetch('https://meproweblite.onrender.com/api/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
